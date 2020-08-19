e757324895947d687daa212630df94cbfa915daa
caotl <caotl@neusoft.com>
2020-08-19 16:34:27 +0800

    ioc demo

---
+++ src/main/java/tech/truman/demo/ioc/Car.java
@@ -0,0 +1,11 @@
+package tech.truman.demo.ioc;
+
+public class Car {
+    private String name;
+    private String length;
+    private String width;
+    private String height;
+    private Wheel wheel;
+
+    // 省略其他不重要代码
+}
---
+++ src/main/java/tech/truman/demo/ioc/Wheel.java
@@ -0,0 +1,8 @@
+package tech.truman.demo.ioc;
+
+public class Wheel {
+    private String brand;
+    private String specification ;
+
+    // 省略其他不重要代码
+}
